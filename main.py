067ea30aa0056052175052db0e26966a538a068e
seydabeyza <84277832+seydabeyza@users.noreply.github.com>
2021-09-01 15:59:03 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+
+list1={8,9,3,6,1,10}
+list1.reverse()
+print("Thee reversed list is",list1)
+
+list2={91,61,120,34,76,54,78,87,56,64,345}
+list2.sort()
+print("The sorted list is",list2)
+
